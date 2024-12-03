000515a9f48a98c2ca59cfb119f90ad43be560fe
Santi14121967 <jurado.santiago@gmail.com>
2024-12-03 11:41:31 +0100

    Init project

---
+++ PhotoManager/src/main/java/com/estetica/PhotoManager/PhotoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.estetica.PhotoManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoManagerApplication.class, args);
+	}
+
+}
---
+++ PhotoManager/src/test/java/com/estetica/PhotoManager/PhotoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estetica.PhotoManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
